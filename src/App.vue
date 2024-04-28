aca8e2d00b21ba95ed061a7b098408f95913d752
anonimat0 <andrealonsoarroyo@gmail.com>
2024-04-28 15:35:58 -0500

    feat: footer-webstorm added

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import ToolbarWebmaster from "./public/components/toolbar-webmaster.component.vue";
+import FooterWebmaster from "./public/components/footer-webstorm.component.vue";
+
+export default{
+  components: {ToolbarWebmaster, FooterWebmaster},
+}
+</script>
+<template>
+  <toolbar-webmaster/>
+  <footer-webmaster/>
+</template>
+<style>
+
+</style>
