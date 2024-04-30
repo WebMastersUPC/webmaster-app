03b3fe102cc53525b9f804e223234542e4f20dc8
anonimat0 <andrealonsoarroyo@gmail.com>
2024-04-30 16:23:54 -0500

    fix: clean app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,6 @@
 <script>
-import ToolbarWebmaster from "./public/components/toolbar-webmaster.component.vue";
-import FooterWebmaster from "./public/components/footer-webstorm.component.vue";
-
-export default{
-  components: {ToolbarWebmaster, FooterWebmaster},
-}
 </script>
 <template>
-  <toolbar-webmaster/>
-  <footer-webmaster/>
 </template>
 <style>
-
 </style>
