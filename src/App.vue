01ee7a8f1009bf1e840c346237cdd63ce73dcc8c
anonimat0 <andrealonsoarroyo@gmail.com>
2024-04-30 16:22:36 -0500

    fix: clean app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,8 @@
 <script>
-import PostulateProjectPage from "./postulate-project/pages/postulate-project-page.component.vue";
-
-export default{
-  components: {PostulateProjectPage}
-}
 </script>
 
 <template>
-  <postulate-project-page/>
 </template>
 
-<style scoped>
-
+<style>
 </style>
