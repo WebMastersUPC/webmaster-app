19e06b24657b29571f68c7391bdbc582052ef3e5
JosephLlacchua <142326204+JosephLlacchua@users.noreply.github.com>
2024-04-30 16:25:49 -0500

    fix: cleaning App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <script>
 
-import {defineComponent} from "vue";
-import FormComponent from "./components/message/form-component.vue";
 
-export default defineComponent({
-  components: { FormComponent}
-})
 </script>
 
 
 <template>
-  <form-component></form-component>
+
 </template>
 
-<style >
+<style scoped >
 
 </style>
