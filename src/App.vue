51a45bb1b1ac73f0bf060524933c0b8c1c549daa
JosephLlacchua <142326204+JosephLlacchua@users.noreply.github.com>
2024-04-30 16:27:18 -0500

    fix: cleaning App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,9 @@
 <script>
 
-import {defineComponent} from "vue";
-import CreateProjectComponent from "./components/create/create-project-component.vue";
-
-export default defineComponent({
-  components: {CreateProjectComponent}
-})
 </script>
 
 
 <template>
-  <create-project-component></create-project-component>
 
 </template>
 
