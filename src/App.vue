67c0c32e488cc1880d2422f4984c527b1a901573
Purak189 <106536136+Purak189@users.noreply.github.com>
2024-04-30 16:29:36 -0500

    fix: cleaning App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,8 @@
 <script setup>
 
-import ReviewDeliverable from "./deliverables/pages/review-deliverable.component.vue";
-import ConfirmationDialog from "./deliverables/components/confirmationDialog.component.vue";
-import CreateDeliverableCard from "./deliverables/components/create-deliverable-card.component.vue";
-import CreateDeliverable from "./deliverables/pages/create-deliverable.component.vue";
-
 </script>
 
 <template>
-
-  <create-deliverable></create-deliverable>
 
 </template>
 
