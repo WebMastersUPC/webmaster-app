c0d3a75189ff7e779cf11a6aeee9faf67189c990
Purak189 <106536136+Purak189@users.noreply.github.com>
2024-04-30 16:23:11 -0500

    fix: cleaning App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,8 @@
 <script setup>
 
-import DeveloperCard from "./search-developer/components/developer-card.component.vue";
-import SearchDeveloperList from "./search-developer/pages/search-developer-list.component.vue";
-
 </script>
 
 <template>
-
-  <search-developer-list></search-developer-list>
 
 </template>
 
