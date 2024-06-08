27b634f9a388896b7d418673b75bc9f8d515d6d8
Purak189 <106536136+Purak189@users.noreply.github.com>
2024-06-08 13:37:08 -0500

    feat: loggin with "Enter" keycap

--- src/authentication/components/login-card.component.vue
+++ src/authentication/components/login-card.component.vue
@@ -25,32 +25,31 @@ export default {
     </div>
 
     <div class="flex justify-content-center align-items-center text-center" style="height: 80vh;">
-      <pv-card aria-label="Login Form" class=" flex border-card shadow-4 px-6 ">
-        <template #title>
-          <div>
-            <img src="../../assets/logo.png" alt="Webmaster's logo" width="210" class="-m-2 ">
-          </div>
+<pv-card aria-label="Login Form" class=" flex border-card shadow-4 px-6 ">
+  <template #title>
+    <div>
+      <img src="../../assets/logo.png" alt="Webmaster's logo" width="210" class="-m-2 ">
+    </div>
+  </template>
+  <template #content>
+    <div class=" flex flex-column gap-3 align-items-center">
+      <div class="flex flex-column gap-4 ">
+        <pv-inputText aria-label="User input field" type="text" v-model="Mail" placeholder="Usuario" class="border-round-3xl" @keyup.enter="login"/>
+        <pv-password aria-label="Password input field" v-model="Password" :feedback="false" placeholder="Contraseña" class="border-round-3xl" @keyup.enter="login"/>
+      </div>
 
-        </template>
-        <template #content>
-          <div class=" flex flex-column gap-3 align-items-center">
-            <div class="flex flex-column gap-4 ">
-              <pv-inputText aria-label="User input field" type="text" v-model="Mail" placeholder="Usuario" class="border-round-3xl"/>
-              <pv-password aria-label="Password input field" v-model="Password" :feedback="false" placeholder="Contraseña" class="border-round-3xl"/>
-            </div>
-
-            <pv-button aria-label="Login button" label="Iniciar sesion" class="border-round-xl w-10rem bg-blue-600 text-lg" @click="login()"/>
-          </div>
-          <div class="color-auth font-bold">
-            <p>
-              <router-link aria-label="Go to password recovery" to="/password-recovery" class="text-purple-500 text-sm">¿Olvidaste tu contraseña?</router-link>
-            </p>
-            <p class="-mt-1">
-              <router-link aria-label="Go to registration page" to="/register" class="text-purple-500 text-sm">Crear cuenta</router-link>
-            </p>
-          </div>
-        </template>
-      </pv-card>
+      <pv-button aria-label="Login button" label="Iniciar sesion" class="border-round-xl w-10rem bg-blue-600 text-lg" @click="login"/>
+    </div>
+    <div class="color-auth font-bold">
+      <p>
+        <router-link aria-label="Go to password recovery" to="/password-recovery" class="text-purple-500 text-sm">¿Olvidaste tu contraseña?</router-link>
+      </p>
+      <p class="-mt-1">
+        <router-link aria-label="Go to registration page" to="/register" class="text-purple-500 text-sm">Crear cuenta</router-link>
+      </p>
+    </div>
+  </template>
+</pv-card>
     </div>
   </div>
 
