10ee17711770a8ed838586d031188cd16c9bef32
anonimat0 <andrealonsoarroyo@gmail.com>
2024-06-20 00:03:39 -0500

    feat: project service added

--- src/shared/models/project.model.js
+++ src/shared/models/project.model.js
@@ -1,16 +1,29 @@
 export class ProjectEntity{
-    name;
+    nameProject;
     type;
-    description;
+    descriptionProject;
+    languages = [];
+    frameworks = [];
+    budget;
+    methodologies = [];
+    enterprise_id;
+    applicants_id = [];
+    developer_id;
     progress;
     started;
-    candidates = [];
-    constructor(name, type, description, progress, started, applicants){
-        this.name = name;
+    constructor(name, type, description, languages, frameworks, budget, methodologies, enterprise_id,
+                applicants, developer_id, progress, started,){
+        this.nameProject = name;
         this.type = type;
-        this.description = description;
+        this.descriptionProject = description;
+        this.languages = languages;
+        this.frameworks = frameworks;
+        this.budget = budget;
+        this.methodologies = methodologies;
+        this.enterprise_id = enterprise_id;
+        this.applicants_id = applicants;
+        this.developer_id = developer_id;
         this.progress = progress;
         this.started = started;
-        this.candidates = applicants;
     }
 }
