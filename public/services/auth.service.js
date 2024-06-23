4e8c823679d9b383dcf2d065450cfb550b4e0769
JosephLlacchua <142326204+JosephLlacchua@users.noreply.github.com>
2024-06-23 17:02:49 -0500

    feat: add API connect for user relationship support requests

--- public/services/auth.service.js
+++ public/services/auth.service.js
@@ -21,10 +21,18 @@ export class AuthService{
     //  getDevInfoByID(id){
     //      return http.get(`Profile/Developers/${id}`)
     // }
+    //user_id = localStorage.getItem('userId');
 
-    async authenticate(Mail, Password) {
-        const response = await http.get(`/Auth/sign-in/${Mail}&${Password}`);
-        return response.data;
+    async authenticate(mail, password) {
+        try {
+            const response = await http.get(`/Auth/sign-in/${mail}&${password}`);
+            //const userId = response.data.user_id; // Obtener el id del usuario desde la respuesta
+            //localStorage.setItem('userId', userId); // Guardar el userId en localStorage
+            return response.data; 
+        } catch (error) {
+            console.error('Error en la autenticación:', error);
+            throw error; // Propagar el error para que el manejador lo gestione
+        }
     }
 
     async registerDeveloper(Developer) {
--- public/services/support.service.js
+++ public/services/support.service.js
@@ -1,18 +1,29 @@
 ﻿import axios from 'axios';
 
 const http = axios.create({
-    baseURL: 'http://localhost:5127/api/v1', 
+    baseURL: 'http://localhost:5127/api/v1',
+   /* headers: {
+        'Authorization': `Bearer ${localStorage.getItem('token')}`
+    }
+
+    */
 });
 
 export default {
-    async getAllSupportRequests() {
-        const response = await http.get('/SupportRequest');
-        return response.data;
-    },
-
     async createSupportRequest(supportRequest) {
-        const response = await http.post('/SupportRequest', supportRequest);
-        return response.data;
+        try {
+            console.log(supportRequest); // Imprime los datos de la solicitud
+            const response = await http.post('/SupportRequest', supportRequest, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                }
+            });
+            return response.data;
+        } catch (error) {
+            console.error('Error creating support request:', error);
+            console.log(error.response); // Imprime la respuesta de error para obtener más detalles
+            throw error; // Propaga el error para que pueda ser manejado en el componente Vue
+        }
     },
 
     async updateSupportRequest(id, supportRequest) {
